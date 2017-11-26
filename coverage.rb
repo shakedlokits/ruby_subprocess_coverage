e452d040130c21bf430adb8e2ad2761882563b2b
Shaked Lokits <shaked.lokits@gmail.com>
2017-11-26 15:58:45 +0200

    fixed env variable access

--- coverage.rb
+++ coverage.rb
@@ -1,33 +1,31 @@
 # Perform coverage conditionally
-if( ENV['COVERAGE'] )
-
-  # Load configuration file
-  project_folder_name = /subRuby/
-  report_directory = '/coverage'
-  analysis_name = 'ruby_subprocess_coverage'
-  config = YAML.load_file( ENV['RUBY_COVERAGE_CONFIG'] )
+if( ENV['RUBY_COVERAGE_CONFIG'] )
 
   # Importing relavant libraries
   require 'yaml'
   require 'simplecov'
   require 'simplecov-rcov'
 
+  # Load configuration file
+  config = YAML.load_file( ENV['RUBY_COVERAGE_CONFIG'] )
+
   # Configuring and initializing analysis
   SimpleCov.start do
 
     # Setting workspace configuration
     root Dir.pwd
-    coverage_dir config.report_directory
+    coverage_dir config['report_directory']
 
     # Setting analysis formatting
-    command_name config.analysis_name
+    command_name config['analysis_name']
     formatters = SimpleCov::Formatter::RcovFormatter
 
     # Adding project filter
     # Without this we get every library called
     filters.clear
     add_filter do |src|
-            !(src.filename =~ /^#{SimpleCov.root}/) unless src.filename =~ config.project_folder_name
+            !(src.filename =~ /^#{SimpleCov.root}/) unless src.filename =~ Regexp.new(config['project_folder_name'])
     end
   end
 end
+
